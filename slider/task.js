ea923c2dc95487cd2fc02fabc77653c7d0e2ba95
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-05-30 10:36:27 +0500

    little refactoring

--- slider/task.js
+++ slider/task.js
@@ -1,13 +1,13 @@
-let activeNumber
+let activeNumber;
 prevArrow = document.querySelector('.slider__arrow_prev')
 nextArrow = document.querySelector('.slider__arrow_next')
-sliders = document.querySelectorAll('.slider__item')
-slidersArr = Array.from(sliders)
-slidersArr.forEach((element, index) => {
-    console.log(element, index)
-    //if (element.className.incudes('slider__item_active')) activeNumber = index
+sliders = Array.from(document.querySelectorAll('.slider__item'))
+sliders.forEach((element, index) => {
+    console.log(element.className)
+    // if (element.className.incudes('slider__item_active')) activeNumber = index //Uncaught TypeError: element.className.incudes is not a function 
     if (element.className == 'slider__item slider__item_active') activeNumber = index
 });
+
 prevArrow.onclick = () => {
     sliders[activeNumber].className = 'slider__item'
     activeNumber = (activeNumber + 4) % 5
